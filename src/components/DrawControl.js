7dd0a6ee1afffeea5023fa492819f375a00bd328
professional-pigeon <kyle.kayperez@gmail.com>
2021-09-17 16:31:07 -0700

    added one line

--- src/components/DrawControl.js
+++ src/components/DrawControl.js
@@ -10,11 +10,7 @@ function MusicControl() {
 return (
       <div>
         <svg width={1000} height={500}>
-          <line x1={10} y1={10} x2={500} y2={10} style={stroke: black}></line>
-          <line x1={10} y1={20} x2={500} y2={20} style={stroke: black}></line>
-          <line x1={10} y1={30} x2={500} y2={30} style={stroke: black}></line>
-          <line x1={10} y1={40} x2={500} y2={40} style={stroke: black}></line>
-          <line x1={10} y1={50} x2={500} y2={50} style={stroke: black}></line>
+          <line x1={10} y1={10} x2={500} y2={10} stroke={"black"}></line>
         </svg>
       </div>
   )
