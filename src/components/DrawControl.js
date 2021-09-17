37e1ef72ccefd82e33ec65662600f5fc22d71165
professional-pigeon <kyle.kayperez@gmail.com>
2021-09-17 16:32:42 -0700

    added several lines

--- src/components/DrawControl.js
+++ src/components/DrawControl.js
@@ -11,6 +11,10 @@ return (
       <div>
         <svg width={1000} height={500}>
           <line x1={10} y1={10} x2={500} y2={10} stroke={"black"}></line>
+          <line x1={10} y1={20} x2={500} y2={20} stroke={"black"}></line>
+          <line x1={10} y1={30} x2={500} y2={30} stroke={"black"}></line>
+          <line x1={10} y1={40} x2={500} y2={40} stroke={"black"}></line>
+          <line x1={10} y1={50} x2={500} y2={50} stroke={"black"}></line>
         </svg>
       </div>
   )
