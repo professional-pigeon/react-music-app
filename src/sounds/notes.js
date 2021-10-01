ede39f94d8fae2c3c551607c5736f36776386ab0
professional-pigeon <kyle.kayperez@gmail.com>
2021-10-01 14:40:44 -0700

    noteCreator makes 96 notes over 8 octaves

--- src/sounds/notes.js
+++ src/sounds/notes.js
@@ -6,15 +6,16 @@ function noteCreator(startingFrequency) {
   let noteNum = 10
   let noteArray = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",]
   console.log(noteArray[noteNum] + "-" + octave)
-  while (i < 4) {
+  while (i < 97) {
     frequency = frequency * (Math.pow(2, (1/12)))
     output[noteArray[noteNum] + "-" + octave] = (frequency)
     if (noteNum === 11) {
       noteNum = 0
+      octave++
     } else {
       noteNum++
     }
-   	i++
+    i++
   }
   return output
 }
