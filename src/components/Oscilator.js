1310b21c741abc776340cfe4a7d41d79e764641a
professional-pigeon <kyle.kayperez@gmail.com>
2021-10-05 09:22:44 -0700

    add piano audio sprite

---
+++ src/components/Oscilator.js
@@ -0,0 +1,24 @@
+function playOscillator(freq) {
+  let audioContext = new window.AudioContext();
+  let osc = audioContext.createOscillator();
+  let mainGainNode = audioContext.createGain();
+  mainGainNode.connect(audioContext.destination);
+  mainGainNode.gain.value = 0.5
+
+  // if (type == "custom") {
+  //   osc.setPeriodicWave(customWaveform);
+  // } else {
+  //   osc.type = type;
+  // }
+  osc.type = 'triangle';
+  osc.frequency.value = freq;
+
+  osc.connect(mainGainNode)
+  console.log(osc)
+  osc.start();
+  osc.stop(.5)
+  console.log("here")
+  return osc;
+}
+
+export default playOscillator
