669da40abb9bfc03e3a609e932cfe96d4cc659a3
professional-pigeon <kyle.kayperez@gmail.com>
2021-10-06 09:08:08 -0700

    selecting notes highlights the piano

--- src/components/PianoVisual.js
+++ src/components/PianoVisual.js
@@ -5,13 +5,13 @@ function PianoVisual(props) {
   const notes = Object.keys(props.pianoNotes._sprite)
   let displayNotes = props.displayNotes
   let keys = []
-  // notes.forEach(function(note){
-  //   if (displayNotes.includes(note) === true) {
-  //     keys.push(<Col className="keys-highlight">{note}</Col>)
-  //   } else {
-  //     keys.push(<Col className="keys">{note}</Col>)
-  //   }
-  // })
+  notes.forEach(function(note){
+    if (displayNotes.includes(note) === true) {
+      keys.push(<Col className="keys-highlight">{note}</Col>)
+    } else {
+      keys.push(<Col className="keys">{note}</Col>)
+    }
+  })
   
 
   return (
