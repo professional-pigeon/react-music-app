243c543a2a5555c88463525f8462e104666d10e3
professional-pigeon <kyle.kayperez@gmail.com>
2022-02-21 17:56:07 -0800

    add notes and player class

--- src/components/display/NoteVisual.js
+++ src/components/display/NoteVisual.js
@@ -42,7 +42,7 @@ function NoteVisual(props) {
   }
 
 return (
-  <div style={{ borderStyle: 'solid', height: 'fit-content' }} id="buttonRow" className='sub-main'>
+  <div style={{ borderStyle: 'solid', height: 'fit-content' }} id="buttonRow" className='sub-main notes'>
     {createInstrumentList(props.playInstrument)}
   </div>
 )
