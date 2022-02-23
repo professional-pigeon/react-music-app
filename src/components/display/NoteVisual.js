cf8f4b4fae082a258f5df029dd9d851511393ab1
professional-pigeon <kyle.kayperez@gmail.com>
2022-02-22 17:03:32 -0800

    change note visualization to divs instead of bootstrap rows and cols

--- src/components/display/NoteVisual.js
+++ src/components/display/NoteVisual.js
@@ -13,31 +13,56 @@ function NoteVisual(props) {
         }
       })
     })
-    return createRows(instruments.sort(), arrayOfNotes)
+    return createDivs(instruments.sort(), arrayOfNotes)
   }
 
   function createRows(instruments, arrayOfNotes) {
     let rowArray = []
     instruments.forEach(function(instrument, key) {
-      if (instrument.length > 3) {
-        let colArray = []
-        for (let i = 0; i < arrayOfNotes.length; i++) {
-          let checkArray = arrayOfNotes[i]
-          if (checkArray.includes(instrument)) {
-            colArray.push(<Col sm="auto" key={key + i}><Button variant="primary" className="selected" onClick={() => props.removeInstrument(instrument, i)}></Button></Col>)
-          } else {
-            colArray.push(<Col sm="auto" key={key + i}><Button variant="warning" className="unselected" onClick={() => props.addInstrument(instrument, i)}></Button></Col>)
-          }
+      let colArray = []
+      for (let i = 0; i < arrayOfNotes.length; i++) {
+        let checkArray = arrayOfNotes[i]
+        if (checkArray.includes(instrument)) {
+          colArray.push(<Col sm="auto" key={key + i}><Button variant="primary" className="selected" onClick={() => props.removeInstrument(instrument, i)}></Button></Col>)
+        } else {
+          colArray.push(<Col sm="auto" key={key + i}><Button variant="warning" className="unselected" onClick={() => props.addInstrument(instrument, i)}></Button></Col>)
         }
-        rowArray.push(
-          <Row className="gutter" id="instrument" xs="auto" key={instrument}>
-            <Col xs={1} key="instrument">{instrument}</Col>
-            {colArray}
-          </Row>
-        )
       }
+      rowArray.push(
+        <Row className="gutter" id="instrument" xs="auto" key={instrument}>
+          <Col xs={1} key="instrument">{instrument}</Col>
+          {colArray}
+        </Row>
+      )
     })
     return rowArray
+  }
+
+  function createDivs(instruments, arrayOfNotes) {
+    let outerDivArray = []
+    instruments.forEach(function(instrument, key) {
+      let innerDivArray = []
+      let buttonSequence = []
+      for (let i = 1; i < 17; i++) {
+        let checkArray = arrayOfNotes[i - 1]
+        if (checkArray.includes(instrument)) {
+          buttonSequence.push(<Button key={i} variant="primary" className="selected" onClick={() => props.removeInstrument(instrument, i - 1)}></Button>)
+        } else {
+          buttonSequence.push(<Button key={i} variant="warning" className="unselected" onClick={() => props.addInstrument(instrument, i - 1)}></Button>)
+        }
+        if (i % 4 === 0) {
+          innerDivArray.push(<div id='inner-div' key={i + "bar"}>{buttonSequence}</div>)
+          buttonSequence = []
+        }
+      }
+      outerDivArray.push(
+        <div id="instrument-row" key={instrument}>
+          <div key={instrument + "name"} id="instrument-name">{instrument}</div>
+          {innerDivArray}
+        </div>
+      )
+    })
+    return outerDivArray
   }
 
 return (
@@ -55,3 +80,5 @@ NoteVisual.propTypes = {
 }
 
 export default NoteVisual
+
+
